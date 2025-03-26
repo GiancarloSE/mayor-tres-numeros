4977761bbdbbc98bd5fa060d0e1d29c69c7ad678
74895735 <74895735@continental.edu.pe>
2025-03-26 11:25:02 -0500

    Inplementacion del mayor de tres numero

---
+++ mayor-tres-numeros.py
@@ -0,0 +1,24 @@
+"""
+Determina el mayor de tres numeros ingresados por el teclado
+"""
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+numero3 = int(input("Ingrese el tercer numero: "))
+
+if numero1 > numero2:
+    temporal = numero1
+    numero1 = numero2
+    numero3 = temporal
+
+if numero2 > numero3:
+    temporal = numero2
+    numero2 = numero3
+    numero1 = temporal
+
+if numero3 > numero1:
+    temporal = numero3
+    numero3 = numero2
+    numero3 = temporal
+
+print(f"numero ordenados: {numero1},{numero2},{numero3}")
+print(f"El mayor es {numero3}")
